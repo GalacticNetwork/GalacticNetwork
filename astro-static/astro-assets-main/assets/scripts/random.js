1ed159c0c62ee31ae89e5e2f2936d506f26f0ff2
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-04-09 18:46:25 -0700

    unzipgit

---
+++ astro-static/astro-assets-main/assets/scripts/random.js
@@ -0,0 +1,62 @@
+function randomLink() {
+  var links = [
+    "/gs/files.html?file=fnacr&type=local",
+    "/gs/files.html?file=fruit-ninja&type=local",
+"/gs/files.html?file=four-colors&type=local",
+"/gs/files.html?file=bitlife&type=local",
+"/gs/files.html?file=10-minutes-till-dawn&type=3kh0",
+"/gs/files.html?file=granny&type=local",
+"/gs/files.html?file=justfall.lol&type=local",
+"/gs/files.html?file=tiq-book&type=local",
+"/gs/files.html?file=snake&type=local",
+"/gs/files.html?file=superhot&type=local",
+"/gs/files.html?file=tanuki-sunset&type=local",
+"/gs/files.html?file=drift-boss/inde.html&type=local",
+"/gs/files.html?file=drift-hunters/inde.html&type=local",
+"/gs/files.html?file=fnaf/1/2.html&type=local",
+"/gs/files.html?file=getting-over-it&type=local",
+"/gs/files.html?file=subway-surfers&type=3kh0",
+"/gs/files.html?file=stumble&type=local",
+"/gs/files.html?file=pvz&type=local",
+"/gs/files.html?file=run-3&type=local",
+"/gs/files.html?file=snowball.io&type=local",
+"/gs/files.html?file=sm64&type=local",
+"/gs/files.html?file=shell-shockers&type=local",
+"/gs/files.html?file=ovo&type=3kh0",
+"/gs/files.html?file=fortnite&type=local",
+"/gs/files.html?file=rblx&type=local",
+"/gs/files.html?file=angry-birds&type=local",
+"/gs/files.html?file=among-us&type=local",
+"/gs/files.html?file=oldvsnew&type=local",
+"/gs/files.html?file=1v1-battle&type=local",
+    "/gs/files.html?file=tiny-fishing&type=local",
+    "/gs/files.html?file=vex/7&type=local",
+    "/gs/files.html?file=we-become-what-we-behold&type=local",
+    "/gs/files.html?file=slope-2&type=local",
+    "/gs/files.html?file=retro-bowl&type=local",
+    "/gs/files.html?file=retro-bowl-college&type=local",
+    "/gs/files.html?file=mario-combat&type=local",
+    "/gs/files.html?file=johnny-trigger&type=local",
+    "/gs/files.html?file=fire-boy-and-water-girl&type=local",
+    "/gs/files.html?file=FNF&type=local",
+    "/gs/files.html?file=football-legends&type=local",
+    "/gs/files.html?file=fnaf/3/2.html&type=local",
+    "/gs/files.html?file=four-colors&type=local",
+    "/gs/files.html?file=minecraft-classic&type=local",
+    "/gs/files.html?file=osu&type=local",
+    "/gs/files.html?file=neal.fun&type=local",
+    "/gs/files.html?file=parking-fury/3&type=local",
+    "/gs/files.html?file=pokemon/emerald/inde.html&type=local",
+    "/gs/files.html?file=slope-2&type=local",
+    "/gs/files.html?file=snake&type=local",
+    "/gs/files.html?file=snowrider3d&type=local",
+
+  ];
+  var randomIndex = Math.floor(Math.random() * links.length);
+
+
+  window.location.href = links[randomIndex];
+}
+
+
+
