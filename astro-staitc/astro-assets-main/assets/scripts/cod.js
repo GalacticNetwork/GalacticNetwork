21e1fcd4a24c116622d9355b2fe682bb6c1663f1
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-04-09 18:58:10 -0700

    unzipgit

---
+++ astro-staitc/astro-assets-main/assets/scripts/cod.js
@@ -0,0 +1,15 @@
+function ab() {
+    var urlObj = new window.URL(window.location.href);
+      win = window.open();
+      win.document.body.style.margin = "0";
+      win.document.body.style.height = "100vh";
+      var iframe = document.createElement("iframe");
+      iframe.style.border = "none";
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.margin = "0";
+      iframe.referrerpolicy = "no-referrer";
+      iframe.allow = "fullscreen";
+      iframe.src = "https://ji4sg3.sse.codesandbox.io/client/https://nowgg.nl/play/devsisters-corporation/3475/cookie-run.html";
+      win.document.body.appendChild(iframe);
+}
